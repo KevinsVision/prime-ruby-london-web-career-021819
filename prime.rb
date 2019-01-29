fd1072719de6283d664ea118ea2129bd80f24778
Kevin Lopez Cifuentes <us.kevin@icloud.com>
2019-01-29 18:41:21 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 # Add  code here!
 def prime?(number)
   if !number.Integer?
-    return false
+    false
   if number < 2 
-    return false
+    false
   if number == 2
     return true
   (2..number-1).each {|in| return false if number % in == 0}
